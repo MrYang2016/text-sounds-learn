93717d46eb0411dcd1a259e51e579fa21d6ff788
羊锡贵 <yangxigui@corp.polyv.net>
2024-04-06 19:46:08 +0800

    build

--- pages/api/add-text.ts
+++ pages/api/add-text.ts
@@ -34,3 +34,4 @@ export default async function handler(req: Request, res: Response) {
   }
 }
 
+export const runtime = 'edge'
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { startTTS } from "../../tool/TTS";
 
 export default function handler(req: Request, res: Response) {
-  startTTS('You are welcome to practice speaking!!');
   res.status(200).json({ text: 'Hello' });
 }
+
+export const runtime = 'edge'
